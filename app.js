76fadc78c7eda143c8e0955e801ca082b47ab3d1
EliasFernandes03 <elias.neto@dunasystem.com>
2023-12-12 12:35:53 -0300

    refactor

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads'); // Define o diretório onde os arquivos serão armazenados temporariamente
+    cb(null, './uploads'); 
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`); // Define o nome do arquivo
+    cb(null, `${Date.now()}_${file.originalname}`); 
   },
 });
 const upload = multer({ storage });
@@ -23,28 +23,24 @@ app.post('/converter', upload.single('wordFile'), async (req, res) => {
     }
 
     const filePath = req.file.path;
-
-    // Verifica se o arquivo enviado é um arquivo Word (opcional)
     const fileExtension = path.extname(filePath);
     if (fileExtension !== '.docx' && fileExtension !== '.doc') {
-      // Se o arquivo não for um arquivo Word, você pode lidar com isso aqui
-      fs.unlinkSync(filePath); // Remove o arquivo não suportado
+      fs.unlinkSync(filePath); 
       return res.status(400).send('Somente arquivos do Word são suportados');
     }
 
-    // Lógica para converter o arquivo Word para PDF usando pdfkit
-    const pdfDoc = new PDFDocument();
-    const pdfFileName = `${Date.now()}_converted.pdf`;
+    const fileNameWithoutExtension = path.parse(filePath).name; 
+    const pdfFileName = path.join('./uploads', `${fileNameWithoutExtension}.pdf`); 
 
+    
+    const pdfDoc = new PDFDocument();
     pdfDoc.pipe(fs.createWriteStream(pdfFileName));
-    pdfDoc.text(`Conteúdo do arquivo Word: ${filePath}`); // Aqui você pode adicionar o conteúdo do arquivo ao PDF
+    pdfDoc.text(`Conteúdo do arquivo Word: ${filePath}`);
     pdfDoc.end();
 
-    // Remova os arquivos temporários após a conversão
-    fs.unlinkSync(filePath); // Remove o arquivo Word temporário
+    fs.unlinkSync(filePath); 
 
-    // Envie um status 200 OK indicando sucesso
-    res.status(200).send('Arquivo convertido para PDF com sucesso!');
+    res.status(200).send(`Arquivo convertido para PDF com sucesso: ${pdfFileName}`);
   } catch (err) {
     console.error(err);
     res.status(500).send('Erro no servidor');
